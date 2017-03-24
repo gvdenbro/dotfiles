a87711fbb0bbf1b19ef597b5af76cd5dda633343
Gregory Vandenbroucke <gvandenbroucke@caami.be>
2017-03-24 13:34:33 +0100

    enable/disable proxies...

--- proxy_disable.sh
+++ proxy_disable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # comment ~/.bashrc
-echo -e "Preparing to comment ~/.bashrc proxy settings"
+echo -e "Preparing to comment $HOME/.bashrc proxy settings"
 if [ -e "$HOME/.bashrc" ]; then
-    echo -e "Commenting ~/.bashrc proxy settings"
-    sed -i '/^\(http\|https\|ftp\|no\)_proxy/s/^/#/g' ~/.bashrc
-    sed -i '/^\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^/#/g' ~/.bashrc
-    echo -e "Finished commenting ~/.bashrc proxy settings"
+    echo -e "Commenting $HOME/.bashrc proxy settings"
+    sed -i '/^\(http\|https\|ftp\|no\)_proxy/s/^/#/g' "$HOME/.bashrc"
+    sed -i '/^\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^/#/g' "$HOME/.bashrc"
+    echo -e "Finished commenting $HOME/.bashrc proxy settings"
 else
-    echo -e "Not commenting ~/.bashrc proxy settings because file doesn't exist"
+    echo -e "Not commenting $HOME/.bashrc proxy settings because file doesn't exist"
 fi
--- proxy_enable.sh
+++ proxy_enable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # uncomment ~/.bashrc
-echo -e "Preparing to uncomment ~/.bashrc proxy settings"
+echo -e "Preparing to uncomment $HOME/.bashrc proxy settings"
 if [ -e "$HOME/.bashrc" ]; then
-    echo -e "Uncommenting ~/.bashrc proxy settings"
-    sed -i '/^#\(http\|https\|ftp\|no\)_proxy/s/^#//g' ~/.bashrc
-    sed -i '/^#\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^#//g' ~/.bashrc
-    echo -e "Finished uncommenting ~/.bashrc proxy settings"
+    echo -e "Uncommenting $HOME/.bashrc proxy settings"
+    sed -i '/^#\(http\|https\|ftp\|no\)_proxy/s/^#//g' "$HOME/.bashrc"
+    sed -i '/^#\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^#//g' "$HOME/.bashrc"
+    echo -e "Finished uncommenting $HOME/.bashrc proxy settings"
 else
-    echo -e "Not uncommenting ~/.bashrc proxy settings because file doesn't exist"
+    echo -e "Not uncommenting $HOME/.bashrc proxy settings because file doesn't exist"
 fi

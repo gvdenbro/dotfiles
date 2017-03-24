eaf692b6d0c8afcd3d6d83dc61444c80b94fd316
Gregory Vandenbroucke <gvandenbroucke@caami.be>
2017-03-24 16:23:32 +0100

    enable/disable proxies... adding apt support

--- proxy_disable.sh
+++ proxy_disable.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # comment ~/.bashrc
-echo -e "Preparing to comment $HOME/.bashrc proxy settings"
-if [ -e "$HOME/.bashrc" ]; then
-    echo -e "Commenting $HOME/.bashrc proxy settings"
-    sed -i '/^\(http\|https\|ftp\|no\)_proxy/s/^/#/g' "$HOME/.bashrc"
-    sed -i '/^\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^/#/g' "$HOME/.bashrc"
-    echo -e "Finished commenting $HOME/.bashrc proxy settings"
+bashrc="$HOME/.bashrc"
+echo -e "Preparing to comment $bashrc proxy settings"
+if [ -e "$bashrc" ]; then
+    echo -e "Commenting $bashrc proxy settings"
+    sed -i '/^\(http\|https\|ftp\|no\)_proxy/s/^/#/g' "$bashrc"
+    sed -i '/^\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^/#/g' "$bashrc"
+    echo -e "Finished commenting $bashrc proxy settings"
 else
-    echo -e "Not commenting $HOME/.bashrc proxy settings because file doesn't exist"
+    echo -e "Not commenting $bashrc proxy settings because file doesn't exist"
+fi
+
+# comment apt conf proxy settings
+aptconf="/etc/apt/apt.conf"
+echo -e "Preparing to comment $aptconf proxy settings"
+if [ -e "$aptconf" ]; then
+    echo -e "Commenting $aptconf proxy settings"
+    sed -i '/Proxy/s/^/#/g' "$aptconf"
+    echo -e "Finished commenting $aptconf proxy settings"
+else
+    echo -e "Not commenting $aptconf proxy settings because file doesn't exist"
 fi
--- proxy_enable.sh
+++ proxy_enable.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # uncomment ~/.bashrc
-echo -e "Preparing to uncomment $HOME/.bashrc proxy settings"
-if [ -e "$HOME/.bashrc" ]; then
-    echo -e "Uncommenting $HOME/.bashrc proxy settings"
-    sed -i '/^#\(http\|https\|ftp\|no\)_proxy/s/^#//g' "$HOME/.bashrc"
-    sed -i '/^#\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^#//g' "$HOME/.bashrc"
-    echo -e "Finished uncommenting $HOME/.bashrc proxy settings"
+bashrc="$HOME/.bashrc"
+echo -e "Preparing to uncomment $bashrc proxy settings"
+if [ -e "$bashrc" ]; then
+    echo -e "Uncommenting $bashrc proxy settings"
+    sed -i '/^#\(http\|https\|ftp\|no\)_proxy/s/^#//g' "$bashrc"
+    sed -i '/^#\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^#//g' "$bashrc"
+    echo -e "Finished uncommenting $bashrc proxy settings"
 else
-    echo -e "Not uncommenting $HOME/.bashrc proxy settings because file doesn't exist"
+    echo -e "Not uncommenting $bashrc proxy settings because file doesn't exist"
+fi
+
+# uncomment apt conf proxy settings
+aptconf="/etc/apt/apt.conf"
+echo -e "Preparing to uncomment $aptconf proxy settings"
+if [ -e "$aptconf" ]; then
+    echo -e "Uncommenting $aptconf proxy settings"
+    sed -i '/Proxy/s/^#//g' "$aptconf"
+    echo -e "Finished uncommenting $aptconf proxy settings"
+else
+    echo -e "Not uncommenting $aptconf proxy settings because file doesn't exist"
 fi

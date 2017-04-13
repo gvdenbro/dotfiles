93dc5a82465211b4d11c134c8669e459b217eecc
Gregory Vandenbroucke <gvandenbroucke@caami.be>
2017-04-13 20:16:11 +0200

    enable/disable proxies... gsettings... buggy... fixy

--- proxy_enable.sh
+++ proxy_enable.sh
@@ -17,8 +17,8 @@ envconf="/etc/environment"
 echo -e "Preparing to uncomment $envconf proxy settings"
 if [ -e "$envconf" ]; then
     echo -e "Uncommenting $envconf proxy settings"
-    sed -i '/^#\(http\|https\|ftp\|no\)_proxy/s/^#//g' "$envconf"
-    sed -i '/^#\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^#//g' "$envconf"
+    sudo sed -i '/^#\(http\|https\|ftp\|no\)_proxy/s/^#//g' "$envconf"
+    sudo sed -i '/^#\(HTTP\|HTTPS\|FTP\|NO\)_PROXY/s/^#//g' "$envconf"
     echo -e "Finished uncommenting $envconf proxy settings"
 else
     echo -e "Not uncommenting $envconf proxy settings because file doesn't exist"
@@ -29,7 +29,7 @@ aptconf="/etc/apt/apt.conf"
 echo -e "Preparing to uncomment $aptconf proxy settings"
 if [ -e "$aptconf" ]; then
     echo -e "Uncommenting $aptconf proxy settings"
-    sed -i '/Proxy/s/^#//g' "$aptconf"
+    sudo sed -i '/Proxy/s/^#//g' "$aptconf"
     echo -e "Finished uncommenting $aptconf proxy settings"
 else
     echo -e "Not uncommenting $aptconf proxy settings because file doesn't exist"

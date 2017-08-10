c29dd5f81421b7cd042b0ffba89777061dd3f7e0
gvdenbro <gregory.vandenbroucke@gmail.com>
2017-08-10 20:57:43 +0200

    fixes

--- sync/sync-foobar.sh
+++ sync/sync-foobar.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-if [ -d "/media/greg/corsair" ]; then
-    echo "Disk /media/greg/corsair is mounted"
-    rsync -av /home/greg/Documents /home/greg/ebooks /home/greg/Music /home/greg/Pictures /home/greg/Desktop /media/greg/corsair/backup/ --delete
+if [ -d "/media/greg/corsair/backup" ]; then
+    echo "Disk /media/greg/corsair/backup is mounted"
+    rsync -av --inplace /home/greg/Documents /home/greg/ebooks /home/greg/Music /home/greg/Pictures /home/greg/Desktop /media/greg/corsair/backup/. --delete
     exit 0
 else
-    echo "Disk /media/greg/corsair is missing"
+    echo "Disk /media/greg/corsair/backup is missing"
     exit 1
 fi

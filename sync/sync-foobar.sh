c5eaf3f98a5ef9cbb42fb46d32911e64fdad32c4
gvdenbro <gregory.vandenbroucke@gmail.com>
2017-08-12 19:45:43 +0200

    fix

--- sync/sync-foobar.sh
+++ sync/sync-foobar.sh
@@ -2,7 +2,7 @@
 if [ -h "/dev/disk/by-label/corsair" ]; then
     echo "Disk /media/greg/corsair is mounted"
     if [ -d "/media/greg/corsair/backup" ]; then
-        rsync -vhrt --partial --delete /home/greg/Documents /home/greg/Music /home/greg/Pictures /media/greg/corsair/backup/. | tee /media/greg/corsair/backup/last.sync
+        rsync -vhrt --partial --delete /home/greg/Documents /home/greg/Music /home/greg/Pictures /home/greg/Desktop /media/greg/corsair/backup/. | tee /media/greg/corsair/backup/last.sync
     fi
 else
     echo "Disk /media/greg/corsair/backup is missing"

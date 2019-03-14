2d5ca05d66a6df0dcd5bc237a49caf670d1d6da9
gvdenbro <gregory.vandenbroucke@gmail.com>
2019-03-14 14:24:05 +0100

    cheat sheets

--- gerg/.gerg/functions.bash
+++ gerg/.gerg/functions.bash
@@ -1,3 +1,7 @@
-gergmovresize () {
-	ffmpeg -i $1 -c:v libx264 -preset veryslow -crf 28 -c:a copy -pix_fmt yuv420p $1.resized.mkv
+gergmovresize() {
+	ffmpeg -i $1 -c:v libx264 -preset veryslow -crf 28 -c:a copy -pix_fmt yuv420p $1.resized.mkv;
+}
+
+gergcheat() {
+    curl cheat.sh/$1
 }

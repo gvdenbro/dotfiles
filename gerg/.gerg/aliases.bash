fe0024d08d774cb3dd20b9a86a31e58106fe5332
gvdenbro <gregory.vandenbroucke@gmail.com>
2020-06-03 14:32:08 +0200

    updating aliases

--- gerg/.gerg/aliases.bash
+++ gerg/.gerg/aliases.bash
@@ -5,3 +5,5 @@ alias gergcopy='xclip -selection c'
 alias xo="xdg-open"
 alias ffnt='firefox --new-tab'
 alias tmux='TERM=screen-256color tmux'
+alias gergocr='ocrmypdf -l fra+nld+eng --deskew --jobs 4'
+alias pandoc='podman run --rm -v `pwd`:/pandoc docker://docker.io/dalibo/pandocker'

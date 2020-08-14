e035fd514c8ed28a0053e59f475cbbdfb57a1feb
gerg <gerg@home.be>
2020-08-14 13:34:13 +0200

    better video encoding

--- gerg/.gerg/functions.bash
+++ gerg/.gerg/functions.bash
@@ -1,7 +1,7 @@
 export EDITOR='vim'
 
 gergmovresize() {
-	ffmpeg -i $1 -c:v libx264 -preset veryslow -crf 28 -c:a copy -pix_fmt yuv420p $1.resized.mkv;
+	ffmpeg -i $1 -c:v libx264 -preset veryslow -crf 28 -c:a copy -pix_fmt yuv420p ${1%.*}.mkv;
 }
 
 gergcheat() {

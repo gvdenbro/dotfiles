772b49fc388843f5130fa23d1dabf96886149c0e
gerg <gerg@home.be>
2020-09-29 18:37:16 +0200

    adding a docker R alias and a fzf preview alias

--- gerg/.gerg/aliases.bash
+++ gerg/.gerg/aliases.bash
@@ -7,3 +7,5 @@ alias ffnt='firefox --new-tab'
 alias tmux='TERM=screen-256color tmux'
 alias gergocr='ocrmypdf -l fra+nld+eng --deskew --jobs 4'
 alias pandoc='podman run --rm -v `pwd`:/pandoc docker://docker.io/dalibo/pandocker'
+alias gergR="echo 'go to http://localhost:8787 user: rtsudio pwd: rstudiopwd' && docker run --rm -p 8787:8787 -v $(pwd):/home/rstudio -e PASSWORD=rstudiopwd rocker/tidyverse"
+alias gergpreview="fzf --preview-window=right:75%  --preview 'bat --style=numbers --color=always --line-range :500 {}'"

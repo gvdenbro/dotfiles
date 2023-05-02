9579480ca318122ba0ca5f7bbee8b87ccafda140
gerg <gerg@ec.eu>
2023-05-02 15:58:58 +0200

    adding whisper script

--- gerg/.gerg/functions.bash
+++ gerg/.gerg/functions.bash
@@ -11,3 +11,49 @@ gergcheat() {
 ioanpdfbook() {
     convert $1 -compress jpeg -gravity center -background white -resize 1240x1753 -extent 1240x1753 -units PixelsPerInch -density 150x150 $2.pdf
 }
+
+gergrecordandtranscribe() {
+
+    pulse_source="default"
+    language="en"
+    model="large"
+
+    if [ -z "$1" ]; then
+        echo "Usage: $0 [pulse_source] [language] [model]"
+        echo ""
+        echo "  Example:"
+        echo "    $0 $pulse_source $language $model"
+        echo ""
+        echo "No pulse_source specified, using default: $pulse_source"
+    else
+        pulse_source="$1"
+    fi
+
+    if [ -n "$2" ]; then
+        language="$2"
+    fi
+
+    if [ -n "$3" ]; then
+        model="$3"
+    fi
+
+    # Create a unique temporary file name for the output file
+    OUTPUT_FILENAME=$(mktemp)
+
+    # Capture the pulseaudio source and stream it to the temporary wav file
+    parec -d $pulse_source | \
+        sox -t raw -r 44100 -e signed-integer -b 16 -c 2 - -r 16000 $OUTPUT_FILENAME.wav &
+
+    # Capture the process ID of the previous command
+    PID=$!
+
+    # Wait for a Ctrl-C signal
+    trap "kill $PID" INT
+
+    # Wait for the background process to finish
+    wait $PID
+
+    # Execute a command on the output file
+    $HOME/dev/whisper.cpp/main -l $language -m $HOME/dev/whisper.cpp/models/ggml-${model}.bin -f $OUTPUT_FILENAME.wav -osrt
+    ffmpeg -i $OUTPUT_FILENAME.wav -i $OUTPUT_FILENAME.wav.srt -c:a libmp3lame -q:a 6 -map 0:a -map 1:s -c:v copy -f matroska $OUTPUT_FILENAME.mkv
+}
